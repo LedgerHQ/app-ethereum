c4fbb962278d7631dee8156dcff34780fa755053
BTChip github <contact@btchip.com>
2017-04-04 17:31:34 +0200

    Move to new build SDK, add 2FA for ERC-20 tokens Matchpool, Lunyr, Round, TaaS

--- src_genericwallet/tokens.h
+++ src_genericwallet/tokens.h
@@ -23,6 +23,6 @@ typedef struct tokenDefinition_t {
     uint8_t decimals;
 } tokenDefinition_t;
 
-#define NUM_TOKENS 20
+#define NUM_TOKENS 29
 
 extern tokenDefinition_t const TOKENS[NUM_TOKENS];

37f33677c89e66d5b3d5ddac9640ab22f8a6682a
BTChip github <contact@btchip.com>
2017-06-02 16:12:08 +0200

    Update tokens list, fix USB crash on disconnect take 2, bump version

--- src_genericwallet/main.c
+++ src_genericwallet/main.c
@@ -2520,6 +2520,9 @@ void handleApdu(volatile unsigned int *flags, volatile unsigned int *tx) {
                 break;
             }
         }
+        CATCH(EXCEPTION_IO_RESET) {
+            THROW(EXCEPTION_IO_RESET);
+        }
         CATCH_OTHER(e) {
             switch (e & 0xF000) {
             case 0x6000:
@@ -2576,6 +2579,9 @@ void sample_main(void) {
                 }
 
                 handleApdu(&flags, &tx);
+            }
+            CATCH(EXCEPTION_IO_RESET) {
+                THROW(EXCEPTION_IO_RESET);
             }
             CATCH_OTHER(e) {
                 switch (e & 0xF000) {

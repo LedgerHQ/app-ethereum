1686e7033d9c189cd96637ba295cf928346a4aa0
Clément Péron <peron.clem@gmail.com>
2018-12-19 17:02:01 +0100

    remove unused label

--- src_genericwallet/main.c
+++ src_genericwallet/main.c
@@ -1193,7 +1193,7 @@ unsigned int io_seproxyhal_touch_tx_ok(const bagl_element_t *e) {
     tx = 65;
     G_io_apdu_buffer[tx++] = 0x90;
     G_io_apdu_buffer[tx++] = 0x00;
-send:
+
     // Send back the response, do not restart the event loop
     io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, tx);
     // Display back the original UX

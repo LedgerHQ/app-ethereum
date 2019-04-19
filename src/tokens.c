2b6d724302b1d13d1decdcad660584ec2dd91d01
Eric Shin <ywshin@coinplug.com>
2019-04-19 17:56:58 +0900

    Add Metadium(META) support

--- src/tokens.c
+++ src/tokens.c
@@ -1182,4 +1182,6 @@ const tokenDefinition_t const TOKENS_TOMOCHAIN[NUM_TOKENS_TOMOCHAIN] = {};
 
 const tokenDefinition_t const TOKENS_TOBALABA[NUM_TOKENS_TOBALABA] = {};
 
+const tokenDefinition_t const TOKENS_METADIUM[NUM_TOKENS_METADIUM] = {};
+
 #endif

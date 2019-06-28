3330d6a14df3cf2e4f3bcad93f86fdc43674f4ee
ngyam <ngy.am.zs@gmail.com>
2019-06-28 13:58:17 +0200

    add(ewf-support): volta and energywebchain added

--- src/tokens.c
+++ src/tokens.c
@@ -1182,4 +1182,8 @@ const tokenDefinition_t const TOKENS_TOMOCHAIN[NUM_TOKENS_TOMOCHAIN] = {};
 
 const tokenDefinition_t const TOKENS_TOBALABA[NUM_TOKENS_TOBALABA] = {};
 
+const tokenDefinition_t const TOKENS_VOLTA[NUM_TOKENS_VOLTA] = {};
+
+const tokenDefinition_t const TOKENS_EWC[NUM_TOKENS_EWC] = {};
+
 #endif

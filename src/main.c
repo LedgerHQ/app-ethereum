7be349dc71499d1f76f3346a7f9415a046322c2e
Michael Straka <mstraka100@gmail.com>
2020-03-23 17:53:04 -0700

    return app type with version

--- src/main.c
+++ src/main.c
@@ -49,6 +49,8 @@ void ui_idle(void);
 uint32_t set_result_get_publicKey(void);
 void finalizeParsing(bool);
 
+#define APP_TYPE 0x02
+
 #define MAX_BIP32_PATH 10
 
 #define APP_FLAG_DATA_ALLOWED 0x01
@@ -2667,10 +2669,11 @@ void handleGetAppConfiguration(uint8_t p1, uint8_t p2, uint8_t *workBuffer, uint
 #ifndef HAVE_TOKENS_LIST
   G_io_apdu_buffer[0] |= APP_FLAG_EXTERNAL_TOKEN_NEEDED;
 #endif
-  G_io_apdu_buffer[1] = LEDGER_MAJOR_VERSION;
-  G_io_apdu_buffer[2] = LEDGER_MINOR_VERSION;
-  G_io_apdu_buffer[3] = LEDGER_PATCH_VERSION;
-  *tx = 4;
+  G_io_apdu_buffer[1] = APP_TYPE;
+  G_io_apdu_buffer[2] = LEDGER_MAJOR_VERSION;
+  G_io_apdu_buffer[3] = LEDGER_MINOR_VERSION;
+  G_io_apdu_buffer[4] = LEDGER_PATCH_VERSION;
+  *tx = 5;
   THROW(0x9000);
 }
 

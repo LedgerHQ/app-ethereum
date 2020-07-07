8399abb8a4a7d0ea49904068bddfe35bf7ee6683
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2020-07-07 23:54:18 +0200

    Fix broken error reporting + bump version 1.4.3

--- src_features/signTx/cmd_signTx.c
+++ src_features/signTx/cmd_signTx.c
@@ -72,10 +72,10 @@ void handleSign(uint8_t p1, uint8_t p2, uint8_t *workBuffer, uint16_t dataLength
       THROW(0x6A80);
   }
 
-  *flags |= IO_ASYNCH_REPLY;
-
   if (txResult == USTREAM_FINISHED) {
     finalizeParsing(false);
   }
+
+  *flags |= IO_ASYNCH_REPLY;
 }
 

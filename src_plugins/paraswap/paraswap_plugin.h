b34fdfc49e153d8088f59d262accbe3e1b8e876d
pscott <scott.piriou@ledger.fr>
2021-04-29 09:46:51 +0200

    Add .h and split into handlers

---
+++ src_plugins/paraswap/paraswap_plugin.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <string.h>
+#include "eth_plugin_internal.h"
+#include "eth_plugin_handler.h"
+#include "shared_context.h"
+#include "ethUtils.h"
+#include "utils.h"
+#include "withdrawal_index.h"
+
+#define ETH2_DEPOSIT_PUBKEY_OFFSET         0x80
+#define ETH2_WITHDRAWAL_CREDENTIALS_OFFSET 0xE0
+#define ETH2_SIGNATURE_OFFSET              0x120
+#define ETH2_DEPOSIT_PUBKEY_LENGTH         0x30
+#define ETH2_WITHDRAWAL_CREDENTIALS_LENGTH 0x20
+#define ETH2_SIGNATURE_LENGTH              0x60
+
+const uint8_t ETHEREUM_ADDRESS[] = {0xee, 0xee, 0xee, 0xee, 0xee, 0xee, 0xee, 0xee, 0xee, 0xee,
+                                    0xee, 0xee, 0xee, 0xee, 0xee, 0xee, 0xee, 0xee, 0xee, 0xee};
+
+// Returns 1 if corresponding address is the Paraswap address for Ethereum (0xeeeee...).
+#define ADDRESS_IS_ETH(_addr) (!memcmp(_addr, ETHEREUM_ADDRESS, 20))
+
+typedef enum {
+    PARASWAP_SWAP_ON_UNI = 0,
+} paraswapSelector_t;
+
+typedef struct paraswap_parameters_t {
+    uint8_t amount_sent[32];            // corresponds to amountIn
+    uint8_t contract_address_sent[20];  // use constant
+    char ticker_sent[MAX_TICKER_LEN];
+
+    uint8_t amount_received[32];            // corresponds to amountOutMin
+    uint8_t contract_address_received[20];  // use constant
+    char ticker_received[MAX_TICKER_LEN];
+
+    uint8_t valid;
+    uint8_t decimals_sent;      // Not mandatory ?
+    uint8_t decimals_received;  // Not mandatory ?
+    uint8_t selectorIndex;
+    uint8_t num_paths;
+} paraswap_parameters_t;

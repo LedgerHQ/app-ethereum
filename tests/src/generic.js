aa5543e9b76ddc01035b73835d904fd17f442bdc
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2021-07-02 18:12:28 +0200

    Fix tests

--- tests/src/generic.js
+++ tests/src/generic.js
@@ -15,14 +15,24 @@ const sim_options_nanox = {
 };
 
 const Resolve = require("path").resolve;
-const NANOS_ELF_PATH = Resolve("elfs/ethereum_nanos/app.elf");
-const NANOX_ELF_PATH = Resolve("elfs/ethereum_nanox/app.elf");
+const NANOS_ELF_PATH = Resolve("elfs/ethereum_nanos.elf");
+const NANOX_ELF_PATH = Resolve("elfs/ethereum_nanox.elf");
+
+const NANOS_ETH_LIB = { "Ethereum": NANOS_ELF_PATH };
+const NANOX_ETH_LIB = { "Ethereum": NANOX_ELF_PATH };
+
+const NANOS_CLONE_ELF_PATH = Resolve("elfs/ethereum_classic_nanos.elf");
+const NANOX_CLONE_ELF_PATH = Resolve("elfs/ethereum_classic_nanox.elf");
 
 const TIMEOUT = 1000000;
 
 module.exports = {
     NANOS_ELF_PATH,
     NANOX_ELF_PATH,
+    NANOS_ETH_LIB,
+    NANOX_ETH_LIB,
+    NANOS_CLONE_ELF_PATH,
+    NANOX_CLONE_ELF_PATH,
     sim_options_nanos,
     sim_options_nanox,
     TIMEOUT,

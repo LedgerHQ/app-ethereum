63467a4145f61caa29fcc962b85abe0552c9fa94
pscott <scott.piriou@ledger.fr>
2021-08-04 17:41:06 +0200

    Update eip1559 test

--- tests/src/eip1559.test.js
+++ tests/src/eip1559.test.js
@@ -9,7 +9,7 @@ import { expect } from "../jest";
 const {NANOS_ELF_PATH, NANOX_ELF_PATH, sim_options_nanos, sim_options_nanox, TIMEOUT} = require("generic.js");
 
 const ORIGINAL_SNAPSHOT_PATH_PREFIX = "snapshots/eip1559/";
-const SNAPSHOT_PATH_PREFIX = "snapshots/tmp/";
+const SNAPSHOT_PATH_PREFIX = "snapshots/eip1559/";
 
 const ORIGINAL_SNAPSHOT_PATH_NANOS = ORIGINAL_SNAPSHOT_PATH_PREFIX + "nanos/";
 const ORIGINAL_SNAPSHOT_PATH_NANOX = ORIGINAL_SNAPSHOT_PATH_PREFIX + "nanox/";
@@ -25,12 +25,12 @@ test("Transfer nanos eip1559", async () => {
     await sim.start(sim_options_nanos);
 
     let transport = await sim.getTransport();
-    let buffer = Buffer.from("058000002c8000003c80000000000000000000000002f88d0101808207d0871000000000000094cccccccccccccccccccccccccccccccccccccccc80a4693c613900000000000000000000000000000000000000000000000000000000000000fac001a0659425c1533f84bacbc9e119863db012ea8e8d49ec9d0ef208254dd67f0bdfa5a043409e4e89855389fe8fafddd4c58616ff", "hex");
+
+    // From this test: https://github.com/ethereum/tests/blob/5d534e37b80e9310e8c7751f805ca481a451123e/GeneralStateTests/stEIP1559/outOfFunds.json#L35
+    let buffer = Buffer.from("058000002c8000003c80000000000000000000000002f87001018502540be4008502540be40086246139ca800094cccccccccccccccccccccccccccccccccccccccc8000c001a0e07fb8a64ea3786c9a6649e54429e2786af3ea31c6d06165346678cf8ce44f9ba00e4a0526db1e905b7164a858fd5ebd2f1759e22e6955499448bd276a6aa62830", "hex");
 
     // Send transaction
     let tx = transport.send(0xe0, 0x04, 0x00, 0x00, buffer);
-    buffer = Buffer.from("cb3c99990c13e47cf362d63e012b9b", "hex");
-    tx = transport.send(0xe0, 0x04, 0x80, 0x00, buffer);
     let filename;
 
     await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
@@ -41,26 +41,12 @@ test("Transfer nanos eip1559", async () => {
     const expected_review = Zemu.LoadPng2RGB(ORIGINAL_SNAPSHOT_PATH_NANOS + filename);
     expect(review).toEqual(expected_review);
 
-    // Amount 1/3
-    filename = "amount_1.png";
+    // Amount
+    filename = "amount.png";
     await sim.clickRight(SNAPSHOT_PATH_NANOS + filename);
-    const amount_1 = Zemu.LoadPng2RGB(SNAPSHOT_PATH_NANOS + filename);
-    const expected_amount_1 = Zemu.LoadPng2RGB(ORIGINAL_SNAPSHOT_PATH_NANOS + filename);
-    expect(amount_1).toEqual(expected_amount_1);
-
-    // Amount 2/3
-    filename = "amount_2.png";
-    await sim.clickRight(SNAPSHOT_PATH_NANOS + filename);
-    const amount_2 = Zemu.LoadPng2RGB(SNAPSHOT_PATH_NANOS + filename);
-    const expected_amount_2 = Zemu.LoadPng2RGB(ORIGINAL_SNAPSHOT_PATH_NANOS + filename);
-    expect(amount_2).toEqual(expected_amount_2);
-
-    // Amount 3/3
-    filename = "amount_3.png";
-    await sim.clickRight(SNAPSHOT_PATH_NANOS + filename);
-    const amount_3 = Zemu.LoadPng2RGB(SNAPSHOT_PATH_NANOS + filename);
-    const expected_amount_3 = Zemu.LoadPng2RGB(ORIGINAL_SNAPSHOT_PATH_NANOS + filename);
-    expect(amount_3).toEqual(expected_amount_3);
+    const amount = Zemu.LoadPng2RGB(SNAPSHOT_PATH_NANOS + filename);
+    const expected_amount = Zemu.LoadPng2RGB(ORIGINAL_SNAPSHOT_PATH_NANOS + filename);
+    expect(amount).toEqual(expected_amount);
 
     // Address 1/3
     filename = "address_1.png";
@@ -100,14 +86,13 @@ test("Transfer nanos eip1559", async () => {
     await sim.clickBoth();
 
     await expect(tx).resolves.toEqual(
-      Buffer.from([ 38, 111, 56, 157, 21, 50, 15, 5, 1, 56, 53, 38, 237, 3, 222, 145, 124, 20, 33, 39, 22, 240, 154, 38, 45, 188, 152, 67, 16, 134, 165, 219, 73, 13, 201, 148, 183, 185, 114, 48, 187, 53, 253, 246, 254, 194, 244, 216, 255, 76, 251, 139, 254, 178, 166, 82, 195, 100, 199, 56, 255, 3, 60, 5, 221, 144, 0])
-    );
+      Buffer.from([1, 61, 109, 250, 188, 108, 82, 55, 75, 250, 52, 203, 44, 67, 56, 86, 160, 188, 217, 72, 72, 112, 221, 27, 80, 36, 159, 113, 100, 165, 252, 224, 82, 5, 72, 167, 116, 221, 11, 99, 147, 13, 131, 203, 46, 26, 131, 111, 227, 239, 36, 68, 78, 139, 117, 139, 0, 88, 93, 154, 7, 108, 14, 152, 168, 144, 0]));
   } finally {
     await sim.close();
   }
 });
 
-test.skip("Transfer nanox", async () => {
+test("Transfer nanox", async () => {
   jest.setTimeout(TIMEOUT);
   const sim = new Zemu(NANOX_ELF_PATH);
 
@@ -115,7 +100,9 @@ test.skip("Transfer nanox", async () => {
     await sim.start(sim_options_nanox);
 
     let transport = await sim.getTransport();
-    let buffer = Buffer.from("058000002C8000003C800000010000000000000000EB44850306DC4200825208945A321744667052AFFA8386ED49E00EF223CBFFC3876F9C9E7BF6181880018080", "hex");
+
+    // From this test: https://github.com/ethereum/tests/blob/5d534e37b80e9310e8c7751f805ca481a451123e/GeneralStateTests/stEIP1559/outOfFunds.json#L35
+    let buffer = Buffer.from("058000002c8000003c80000000000000000000000002f87001018502540be4008502540be40086246139ca800094cccccccccccccccccccccccccccccccccccccccc8000c001a0e07fb8a64ea3786c9a6649e54429e2786af3ea31c6d06165346678cf8ce44f9ba00e4a0526db1e905b7164a858fd5ebd2f1759e22e6955499448bd276a6aa62830", "hex");
 
     // Send transaction
     let tx = transport.send(0xe0, 0x04, 0x00, 0x00, buffer);
@@ -160,8 +147,7 @@ test.skip("Transfer nanox", async () => {
     await sim.clickBoth();
 
     await expect(tx).resolves.toEqual(
-      Buffer.from([ 38, 111, 56, 157, 21, 50, 15, 5, 1, 56, 53, 38, 237, 3, 222, 145, 124, 20, 33, 39, 22, 240, 154, 38, 45, 188, 152, 67, 16, 134, 165, 219, 73, 13, 201, 148, 183, 185, 114, 48, 187, 53, 253, 246, 254, 194, 244, 216, 255, 76, 251, 139, 254, 178, 166, 82, 195, 100, 199, 56, 255, 3, 60, 5, 221, 144, 0])
-    );
+      Buffer.from([1, 61, 109, 250, 188, 108, 82, 55, 75, 250, 52, 203, 44, 67, 56, 86, 160, 188, 217, 72, 72, 112, 221, 27, 80, 36, 159, 113, 100, 165, 252, 224, 82, 5, 72, 167, 116, 221, 11, 99, 147, 13, 131, 203, 46, 26, 131, 111, 227, 239, 36, 68, 78, 139, 117, 139, 0, 88, 93, 154, 7, 108, 14, 152, 168, 144, 0]));
   } finally {
     await sim.close();
   }

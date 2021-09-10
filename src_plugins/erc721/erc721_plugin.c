49c5dd70a973bbb5f11927156d011351568af7ec
pscott <scott.piriou@ledger.fr>
2021-09-10 08:56:41 +0200

    First draft for erc721 token allowance

--- src_plugins/erc721/erc721_plugin.c
+++ src_plugins/erc721/erc721_plugin.c
@@ -1,150 +1,157 @@
-#include <string.h>
-#include "eth_plugin_internal.h"
-#include "eth_plugin_handler.h"
-#include "shared_context.h"
-#include "ethUtils.h"
-#include "utils.h"
+#include "erc721_plugin.h"
 
-typedef struct erc721_parameters_t {
-    uint8_t selectorIndex;
-    uint8_t address[ADDRESS_LENGTH];
-    uint8_t tokenId[INT256_LENGTH];
-    // tokenDefinition_t *tokenSelf;
-    // tokenDefinition_t *tokenAddress;
-} erc721_parameters_t;
+static void handle_init_contract(void *parameters) {
+    ethPluginInitContract_t *msg = (ethPluginInitContract_t *) parameters;
+    erc721_parameters_t *context = (erc721_parameters_t *) msg->pluginContext;
 
-bool erc721_plugin_available_check() {
-#ifdef HAVE_STARKWARE
-    if (quantumSet) {
-        switch (dataContext.tokenContext.quantumType) {
-            case STARK_QUANTUM_ERC721:
-            case STARK_QUANTUM_MINTABLE_ERC721:
-                return true;
-            default:
-                return false;
+    // Sanity checks
+    if (!allzeroes(msg->pluginSharedRO->txContent->value.value, 32)) {
+        PRINTF("Err: Transaction amount is not 0 for erc721 approval\n");
+        msg->result = ETH_PLUGIN_RESULT_ERROR;
+    } else if (msg->dataSize != APPROVAL_DATA_SIZE) {
+        PRINTF("Invalid erc721 approval data size %d\n", msg->dataSize);
+        msg->result = ETH_PLUGIN_RESULT_ERROR;
+    }
+
+    uint8_t i;
+    for (i = 0; i < NUM_ERC721_SELECTORS; i++) {
+        if (memcmp((uint8_t *) PIC(ERC721_SELECTORS[i]), msg->selector, SELECTOR_SIZE) == 0) {
+            context->selectorIndex = i;
+            break;
         }
     }
-    return false;
-#endif
+
+    // No selector found.
+    if (i == NUM_ERC721_SELECTORS) {
+        PRINTF("Unknown erc721 selector %.*H\n", SELECTOR_SIZE, msg->selector);
+        msg->result = ETH_PLUGIN_RESULT_ERROR;
+        return;
+    }
+
+    msg->result = ETH_PLUGIN_RESULT_OK;
+    switch (context->selectorIndex) {
+        case APPROVAL:
+            context->next_param = FIRST;
+            break;
+        default:
+            PRINTF("Unsupported selector index: %d\n", context->selectorIndex);
+            msg->result = ETH_PLUGIN_RESULT_ERROR;
+            break;
+    }
+}
+
+static void handle_provide_parameter(void *parameters) {
+    ethPluginProvideParameter_t *msg = (ethPluginProvideParameter_t *) parameters;
+    erc721_parameters_t *context = (erc721_parameters_t *) msg->pluginContext;
+
+    PRINTF("erc721 plugin provide parameter %d %.*H\n",
+           msg->parameterOffset,
+           PARAMETER_LENGTH,
+           msg->parameter);
+
+    msg->result = ETH_PLUGIN_RESULT_SUCCESSFUL;
+    switch (msg->parameterOffset) {
+        case SELECTOR_SIZE:
+            memmove(context->address,
+                    msg->parameter + PARAMETER_LENGTH - ADDRESS_LENGTH,
+                    ADDRESS_LENGTH);
+            break;
+        case SELECTOR_SIZE + PARAMETER_LENGTH:
+            memmove(context->tokenId, msg->parameter, PARAMETER_LENGTH);
+            break;
+        default:
+            PRINTF("Unhandled parameter offset\n");
+            msg->result = ETH_PLUGIN_RESULT_ERROR;
+            break;
+    }
+}
+
+static void handle_finalize(void *parameters) {
+    ethPluginFinalize_t *msg = (ethPluginFinalize_t *) parameters;
+    erc721_parameters_t *context = (erc721_parameters_t *) msg->pluginContext;
+
+    msg->tokenLookup1 = msg->pluginSharedRO->txContent->destination;
+    msg->tokenLookup2 = context->address;
+    msg->numScreens = 3;
+    msg->uiType = ETH_UI_TYPE_GENERIC;
+    msg->result = ETH_PLUGIN_RESULT_OK;
+}
+
+static void handle_provide_token(void *parameters) {
+    ethPluginProvideToken_t *msg = (ethPluginProvideToken_t *) parameters;
+    erc721_parameters_t *context = (erc721_parameters_t *) msg->pluginContext;
+
+    msg->result = ETH_PLUGIN_RESULT_OK;
+}
+
+static void handle_contract_id(void *parameters) {
+    ethQueryContractID_t *msg = (ethQueryContractID_t *) parameters;
+
+    strlcpy(msg->name, "NFT", msg->nameLength);
+    strlcpy(msg->version, "Allowance", msg->versionLength);
+    msg->result = ETH_PLUGIN_RESULT_OK;
+}
+
+static void handle_contract_ui(void *parameters) {
+    ethQueryContractUI_t *msg = (ethQueryContractUI_t *) parameters;
+    erc721_parameters_t *context = (erc721_parameters_t *) msg->pluginContext;
+
+    // Collection Name (fallback to contract addr)
+    // ID?
+
+    msg->result = ETH_PLUGIN_RESULT_OK;
+    switch (context->selectorIndex) {
+        case APPROVAL:
+            switch (msg->screenIndex) {
+                case 0:
+                    strlcpy(msg->title, "Collection", msg->titleLength);
+                    getEthDisplayableAddress(msg->pluginSharedRO->txContent->destination,
+                                             msg->pluginSharedRO->txContent->destinationLength,
+                                             msg->msgLength,
+                                             &global_sha3,
+                                             chainConfig->chainId);
+                    break;
+                case 1:
+                    strlcpy(msg->title, "To spend", msg->titleLength);
+                    strlcpy(msg->msg, context->collection_name, msg->msgLength);
+                    break;
+                case 2:
+                    strlcpy(msg->title, "TokenID", msg->titleLength);
+                    snprintf(msg->msg, 70, "0x%.*H", sizeof(context->tokenId), context->tokenId);
+                    break;
+                default:
+                    PRINTF("Unsupported screen index %d\n", msg->screenIndex);
+                    msg->result = ETH_PLUGIN_RESULT_ERROR;
+                    break;
+            }
+            break;
+        default:
+            msg->result = ETH_PLUGIN_RESULT_ERROR;
+            PRINTF("Unsupported selector index %d\n", context->selectorIndex);
+            break;
+    }
 }
 
 void erc721_plugin_call(int message, void *parameters) {
     switch (message) {
         case ETH_PLUGIN_INIT_CONTRACT: {
-            ethPluginInitContract_t *msg = (ethPluginInitContract_t *) parameters;
-            erc721_parameters_t *context = (erc721_parameters_t *) msg->pluginContext;
-            // enforce that ETH amount should be 0
-            if (!allzeroes(msg->pluginSharedRO->txContent->value.value, 32)) {
-                PRINTF("Err: Transaction amount is not 0 for erc721 approval\n");
-                msg->result = ETH_PLUGIN_RESULT_ERROR;
-            } else {
-                size_t i;
-                for (i = 0; i < NUM_ERC721_SELECTORS; i++) {
-                    if (memcmp((uint8_t *) PIC(ERC721_SELECTORS[i]),
-                               msg->selector,
-                               SELECTOR_SIZE) == 0) {
-                        context->selectorIndex = i;
-                        break;
-                    }
-                }
-                if (i == NUM_ERC721_SELECTORS) {
-                    PRINTF("Unknown erc721 selector %.*H\n", SELECTOR_SIZE, msg->selector);
-                    break;
-                }
-                if (msg->dataSize != 4 + 32 + 32) {
-                    PRINTF("Invalid erc721 approval data size %d\n", msg->dataSize);
-                    break;
-                }
-                PRINTF("erc721 plugin init\n");
-                msg->result = ETH_PLUGIN_RESULT_OK;
-            }
+            handle_init_contract(parameters);
         } break;
-
         case ETH_PLUGIN_PROVIDE_PARAMETER: {
-            ethPluginProvideParameter_t *msg = (ethPluginProvideParameter_t *) parameters;
-            erc721_parameters_t *context = (erc721_parameters_t *) msg->pluginContext;
-            PRINTF("erc721 plugin provide parameter %d %.*H\n",
-                   msg->parameterOffset,
-                   32,
-                   msg->parameter);
-            switch (msg->parameterOffset) {
-                case 4:
-                    memmove(context->address, msg->parameter + 32 - 20, 20);
-                    msg->result = ETH_PLUGIN_RESULT_OK;
-                    break;
-                case 4 + 32:
-                    memmove(context->tokenId, msg->parameter, 32);
-                    msg->result = ETH_PLUGIN_RESULT_OK;
-                    break;
-                default:
-                    PRINTF("Unhandled parameter offset\n");
-                    break;
-            }
+            handle_provide_parameter(parameters);
         } break;
-
         case ETH_PLUGIN_FINALIZE: {
-            ethPluginFinalize_t *msg = (ethPluginFinalize_t *) parameters;
-            erc721_parameters_t *context = (erc721_parameters_t *) msg->pluginContext;
-            PRINTF("erc721 plugin finalize\n");
-            msg->tokenLookup1 = msg->pluginSharedRO->txContent->destination;
-            msg->tokenLookup2 = context->address;
-            msg->numScreens = 3;
-            msg->uiType = ETH_UI_TYPE_GENERIC;
-            msg->result = ETH_PLUGIN_RESULT_OK;
+            handle_finalize(parameters);
         } break;
-
         case ETH_PLUGIN_PROVIDE_TOKEN: {
-            ethPluginProvideToken_t *msg = (ethPluginProvideToken_t *) parameters;
-            PRINTF("erc721 plugin provide token dest: %d - address: %d\n",
-                   (msg->token1 != NULL),
-                   (msg->token2 != NULL));
-            // context->tokenSelf = msg->token1;
-            // context->tokenAddress = msg->token2;
-            msg->result = ETH_PLUGIN_RESULT_OK;
+            handle_provide_token(parameters);
         } break;
-
         case ETH_PLUGIN_QUERY_CONTRACT_ID: {
-            ethQueryContractID_t *msg = (ethQueryContractID_t *) parameters;
-            strlcpy(msg->name, "Allowance", msg->nameLength);
-            strlcpy(msg->version, "", msg->versionLength);
-            msg->result = ETH_PLUGIN_RESULT_OK;
+            handle_query_contract_id(parameters);
         } break;
-
         case ETH_PLUGIN_QUERY_CONTRACT_UI: {
-            ethQueryContractUI_t *msg = (ethQueryContractUI_t *) parameters;
-            erc721_parameters_t *context = (erc721_parameters_t *) msg->pluginContext;
-            switch (msg->screenIndex) {
-                case 0:
-                    strlcpy(msg->title, "Contract Name", msg->titleLength);
-                    getEthDisplayableAddress(tmpContent.txContent.destination,
-                                             msg->msg,
-                                             msg->msgLength,
-                                             &global_sha3,
-                                             chainConfig->chainId);
-                    msg->result = ETH_PLUGIN_RESULT_OK;
-                    break;
-
-                case 1:
-                    strlcpy(msg->title, "NFT Contract", msg->titleLength);
-                    getEthDisplayableAddress(context->address,
-                                             msg->msg,
-                                             msg->msgLength,
-                                             &global_sha3,
-                                             chainConfig->chainId);
-                    msg->result = ETH_PLUGIN_RESULT_OK;
-                    break;
-
-                case 2:
-                    strlcpy(msg->title, "TokenID", msg->titleLength);
-                    snprintf(msg->msg, 70, "0x%.*H", 32, context->tokenId);
-                    msg->result = ETH_PLUGIN_RESULT_OK;
-                    break;
-
-                default:
-                    break;
-            }
+            handle_query_contract_ui(parameters);
         } break;
-
         default:
             PRINTF("Unhandled message %d\n", message);
     }
---
+++ src_plugins/erc721/erc721_plugin.h
@@ -0,0 +1,27 @@
+#include <string.h>
+#include "eth_plugin_internal.h"
+#include "eth_plugin_handler.h"
+#include "shared_context.h"
+#include "ethUtils.h"
+#include "utils.h"
+
+#define APPROVAL_DATA_SIZE       SELECTOR_SIZE + 2 * PARAMETER_LENGTH
+#define MAX_COLLECTION_NAME_SIZE 40
+
+typedef enum {
+    APPROVAL,
+    TRANSFER,
+} erc721_selector_t;
+
+typedef enum {
+    FIRST,
+} erc721_selector_field;
+
+typedef struct erc721_parameters_t {
+    uint8_t selectorIndex;
+    uint8_t address[ADDRESS_LENGTH];
+    uint8_t tokenId[INT256_LENGTH];
+    char collection_name[MAX_COLLECTION_NAME_SIZE];
+
+    erc721_selector_field next_param;
+} erc721_parameters_t;

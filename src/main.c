c8bf437424644f01aba21109bbced6973619ee59
Noel Moldvai <noelmoldvai@gmail.com>
2021-09-14 15:15:45 -0700

    Add Shyft support

--- src/main.c
+++ src/main.c
@@ -266,6 +266,9 @@ tokenDefinition_t *getKnownToken(uint8_t *contractAddress) {
         case CHAIN_KIND_POLYGON:
             numTokens = NUM_TOKENS_POLYGON;
             break;
+        case CHAIN_KIND_SHYFT:
+            numTokens = NUM_TOKENS_SHYFT;
+            break;
     }
     for (i = 0; i < numTokens; i++) {
         switch (chainConfig->kind) {
@@ -373,6 +376,9 @@ tokenDefinition_t *getKnownToken(uint8_t *contractAddress) {
                 break;
             case CHAIN_KIND_POLYGON:
                 currentToken = (tokenDefinition_t *) PIC(&TOKENS_POLYGON[i]);
+                break;
+            case CHAIN_KIND_SHYFT:
+                currentToken = (tokenDefinition_t *) PIC(&TOKENS_SHYFT[i]);
                 break;
         }
         if (memcmp(currentToken->address, tmpContent.txContent.destination, ADDRESS_LENGTH) == 0) {

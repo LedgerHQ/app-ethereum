21e2eb712ca5a90bad139409d5c3e8754b461503
pscott <scott.piriou@ledger.fr>
2021-09-14 16:54:15 +0200

    Add UIs

--- src_plugins/erc721/erc721_ui.c
+++ src_plugins/erc721/erc721_ui.c
@@ -1,25 +1,73 @@
 #include "erc721_plugin.h"
 
-static void handle_approval_ui(ethQueryContractUI_t *msg, erc721_parameters_t *context) {
+static void set_approval_ui(ethQueryContractUI_t *msg, erc721_parameters_t *context) {
     switch (msg->screenIndex) {
         case 0:
-            strlcpy(msg->title, "Collection", msg->titleLength);
-            getEthDisplayableAddress(msg->pluginSharedRO->txContent->destination,
+            strlcpy(msg->title, "Allow", msg->titleLength);
+            getEthDisplayableAddress(context->address,
                                      msg->msg,
                                      msg->msgLength,
                                      &global_sha3,
                                      chainConfig->chainId);
             break;
         case 1:
-            strlcpy(msg->title, "To spend", msg->titleLength);
+            strlcpy(msg->title, "To Spend", msg->titleLength);
             strlcpy(msg->msg, context->collection_name, msg->msgLength);
             break;
         case 2:
-            strlcpy(msg->title, "TokenID", msg->titleLength);
-            snprintf(msg->msg, 70, "0x%.*H", sizeof(context->tokenId), context->tokenId);
+            strlcpy(msg->title, "ID", msg->titleLength);
+            uint256_to_decimal(context->tokenId,
+                               sizeof(context->tokenId),
+                               msg->msg,
+                               msg->msgLength);
             break;
         default:
             PRINTF("Unsupported screen index %d\n", msg->screenIndex);
+            msg->result = ETH_PLUGIN_RESULT_ERROR;
+            break;
+    }
+}
+
+static void set_approval_for_all_ui(ethQueryContractUI_t *msg, erc721_parameters_t *context) {
+    switch (msg->screenIndex) {
+        case 0:
+            strlcpy(msg->title, "Allow", msg->titleLength);
+            getEthDisplayableAddress(context->address,
+                                     msg->msg,
+                                     msg->msgLength,
+                                     &global_sha3,
+                                     chainConfig->chainId);
+            break;
+        case 1:
+            strlcpy(msg->title, "To Manage ALL", msg->titleLength);
+            strlcpy(msg->msg, context->collection_name, msg->msgLength);
+            break;
+        default:
+            PRINTF("Unsupported screen index %d\n", msg->screenIndex);
+            msg->result = ETH_PLUGIN_RESULT_ERROR;
+            break;
+    }
+}
+
+static void set_transfer_ui(ethQueryContractUI_t *msg, erc721_parameters_t *context) {
+    switch (msg->screenIndex) {
+        case 0:
+            strlcpy(msg->title, "To", msg->titleLength);
+            getEthDisplayableAddress(context->address,
+                                     msg->msg,
+                                     msg->msgLength,
+                                     &global_sha3,
+                                     chainConfig->chainId);
+            break;
+        case 1:
+            strlcpy(msg->title, "ID", msg->titleLength);
+            uint256_to_decimal(context->tokenId,
+                               sizeof(context->tokenId),
+                               msg->msg,
+                               msg->msgLength);
+            break;
+        default:
+            PRINF("Unsupported screen index %d\n", msg->screenIndex);
             msg->result = ETH_PLUGIN_RESULT_ERROR;
             break;
     }
@@ -32,7 +80,15 @@ void handle_query_contract_ui(void *parameters) {
     msg->result = ETH_PLUGIN_RESULT_OK;
     switch (context->selectorIndex) {
         case APPROVE:
-            handle_approval_ui(msg, context);
+            set_approval_ui(msg, context);
+            break;
+        case SET_APPROVAL_FOR_ALL:
+            set_approval_for_all_ui(msg, context);
+            break;
+        case SAFE_TRANSFER_DATA:
+        case SAFE_TRANSFER:
+        case TRANSFER:
+            set_transfer_ui(msg, context);
             break;
         default:
             msg->result = ETH_PLUGIN_RESULT_ERROR;

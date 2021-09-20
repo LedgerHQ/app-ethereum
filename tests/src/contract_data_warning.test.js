8659a0aa67a790c25f227d88ef01c7866f6c36a6
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2021-09-20 15:34:37 +0200

    Add blind signature test

---
+++ tests/src/contract_data_warning.test.js
@@ -0,0 +1,35 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { expect } from "../jest";
+import { TransportStatusError } from "@ledgerhq/errors";
+import { waitForAppScreen, zemu } from './test.fixture';
+
+test('[Nano S] Try to blind sign with setting disabled', zemu("nanos", async (sim, eth) => {
+  // disable blind signing
+  await sim.navigateAndCompareSnapshots('.', 'nanos_disable_blind_signing', [2, 0, 0, 3, 0]);
+
+  // we can't use eth.signTransaction because it detects that contract data is disabled and fails early
+  let transport = await sim.getTransport();
+  let buffer = Buffer.from("058000002c8000003c800000010000000000000000f849208506fc23ac008303dc3194f650c3d88d12db855b8bf7d11be6c55a4e07dcc980a4a1712d6800000000000000000000000000000000000000000000000000000000000acbc7018080", "hex");
+  let tx = transport.send(0xe0, 0x04, 0x00, 0x00, buffer);
+
+  await expect(tx).rejects.toEqual(new TransportStatusError(0x6a80));
+
+  await waitForAppScreen(sim);
+  await sim.navigateAndCompareSnapshots('.', 'nanos_try_to_blind_sign_with_setting_disabled', [1, 0]);
+}));
+
+test('[Nano X] Try to blind sign with setting disabled', zemu("nanox", async (sim, eth) => {
+  // disable blind signing
+  await sim.navigateAndCompareSnapshots('.', 'nanox_disable_blind_signing', [2, 0, 0, 3, 0]);
+
+  // we can't use eth.signTransaction because it detects that contract data is disabled and fails early
+  let transport = await sim.getTransport();
+  let buffer = Buffer.from("058000002c8000003c800000010000000000000000f849208506fc23ac008303dc3194f650c3d88d12db855b8bf7d11be6c55a4e07dcc980a4a1712d6800000000000000000000000000000000000000000000000000000000000acbc7018080", "hex");
+  let tx = transport.send(0xe0, 0x04, 0x00, 0x00, buffer);
+
+  await expect(tx).rejects.toEqual(new TransportStatusError(0x6a80));
+
+  await waitForAppScreen(sim);
+  await sim.navigateAndCompareSnapshots('.', 'nanox_try_to_blind_sign_with_setting_disabled', [0]);
+}));
---
+++ tests/src/test.fixture.js
@@ -0,0 +1,53 @@
+import Zemu from '@zondax/zemu';
+import Eth from '@ledgerhq/hw-app-eth';
+
+const transactionUploadDelay = 60000;
+
+export async function waitForAppScreen(sim) {
+    await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot(), transactionUploadDelay);
+}
+
+const simOptionsNanoS = {
+    model: 'nanos',
+    logging: true,
+    X11: true,
+    startDelay: 3000,
+    custom: '',
+};
+
+const simOptionsNanoX = {
+    model: 'nanox',
+    logging: true,
+    X11: true,
+    startDelay: 3000,
+    custom: '',
+};
+
+const Resolve = require('path').resolve;
+
+const APP_PATH_NANOS = Resolve('elfs/ethereum_nanos.elf');
+const APP_PATH_NANOX = Resolve('elfs/ethereum_nanox.elf');
+
+export function zemu(device, func) {
+    return async () => {
+        jest.setTimeout(100000);
+        let zemu_args;
+        let sim_options;
+        if(device === "nanos"){
+            zemu_args = [APP_PATH_NANOS];
+            sim_options = simOptionsNanoS;
+        }
+        else{
+            zemu_args = [APP_PATH_NANOX];
+            sim_options = simOptionsNanoX;
+        }
+        const sim = new Zemu(...zemu_args);
+        try {
+            await sim.start(sim_options);
+            const transport = await sim.getTransport();
+            await func(sim, new Eth(transport));
+        } finally {
+            await sim.close();
+        }
+    };
+}

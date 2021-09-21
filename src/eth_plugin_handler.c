d5d7679490beac91c124911575832dddccd0cfb4
pscott <scott.piriou@ledger.fr>
2021-09-21 16:34:13 +0200

    clang-format

--- src/eth_plugin_handler.c
+++ src/eth_plugin_handler.c
@@ -104,7 +104,7 @@ eth_plugin_result_t eth_plugin_perform_init(uint8_t *contractAddress,
                         0) {
                         if ((INTERNAL_ETH_PLUGINS[i].availableCheck == NULL) ||
                             ((PluginAvailableCheck) PIC(
-                                INTERNAL_ETH_PLUGINS[i].availableCheck)) ()) {
+                                INTERNAL_ETH_PLUGINS[i].availableCheck))()) {
                             strlcpy(dataContext.tokenContext.pluginName,
                                     INTERNAL_ETH_PLUGINS[i].alias,
                                     PLUGIN_ID_LENGTH);

5b4b23723666ff344eb85c58d74e7c5d2a4010c0
neozaru <neozaru@mailoo.org>
2021-10-05 18:23:08 +0200

    feat: goerli build

--- src/tokens.h
+++ src/tokens.h
@@ -34,7 +34,7 @@ typedef struct tokenDefinition_t {
 
 #ifdef HAVE_TOKENS_EXTRA_LIST
 
-#define NUM_TOKENS_EXTRA 4
+#define NUM_TOKENS_EXTRA 8
 
 extern tokenDefinition_t const TOKENS_EXTRA[NUM_TOKENS_EXTRA];
 

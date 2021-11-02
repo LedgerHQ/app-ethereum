2fabe9df616129109e96948ca0f4b75c37da170e
DonFinidi <93507613+DonFinidi@users.noreply.github.com>
2021-11-02 01:59:10 +0100

    Add MultiVAC as a variant

--- src/tokens.c
+++ src/tokens.c
@@ -192,4 +192,6 @@ const tokenDefinition_t const TOKENS_MOONRIVER[NUM_TOKENS_MOONRIVER] = {};
 
 const tokenDefinition_t const TOKENS_POLYGON[NUM_TOKENS_POLYGON] = {};
 
+const tokenDefinition_t const TOKENS_POLYGON[NUM_TOKENS_MULTIVAC] = {};
+
 #endif

ef07f77d8a2620c9a06249271606e9d82f167b57
Coline <coline.seguret@ledger.fr>
2022-03-04 13:55:33 +0100

    fix: lint

--- tests/unit/tests/utils.c
+++ tests/unit/tests/utils.c
@@ -7,7 +7,7 @@ int local_strchr(char *string, char ch);
 
 static void null_test_success(void **state) {
     assert_int_equal(local_strchr("salut", 'c'), -1);
-    assert_int_equal(local_strchr("av", 'a'), 0); 
+    assert_int_equal(local_strchr("av", 'a'), 0);
 }
 
 int main(void) {

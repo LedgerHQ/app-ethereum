ad030cdb6a04ac38b2597ce53f10f5053d59c463
Alexandre Paillier <alexandre.paillier@ledger.fr>
2022-05-02 17:27:24 +0200

    Make the dependencies mandatory in the typehash function

--- src_features/signMessageEIP712/type_hash.h
+++ src_features/signMessageEIP712/type_hash.h
@@ -5,7 +5,6 @@
 
 const uint8_t *type_hash(const void *const structs_array,
                          const char *const struct_name,
-                         const uint8_t struct_name_length,
-                         bool with_deps);
+                         const uint8_t struct_name_length);
 
 #endif // TYPE_HASH_H_

8ea34f9f008a32b7bbc7beeeaf41919abb834b8f
Alexandre Paillier <alexandre.paillier@ledger.fr>
2022-06-08 18:31:16 +0200

    Fix hashing of signed negative integers smaller than 256 bits

--- src_features/signMessageEIP712/encode_field.h
+++ src_features/signMessageEIP712/encode_field.h
@@ -8,7 +8,8 @@
 
 #define EIP_712_ENCODED_FIELD_LENGTH    32
 
-void    *encode_integer(const uint8_t *const value, uint8_t length);
+void    *encode_uint(const uint8_t *const value, uint8_t length);
+void    *encode_int(const uint8_t *const value, uint8_t length, uint8_t typesize);
 void    *encode_boolean(const bool *const value, uint8_t length);
 void    *encode_address(const uint8_t *const value, uint8_t length);
 void    *encode_bytes(const uint8_t *const value, uint8_t length);

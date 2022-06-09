e946f80d7d2de94041b8029a7f0c54259d86e81e
Alexandre Paillier <alexandre.paillier@ledger.fr>
2022-06-09 15:21:27 +0200

    Now copies the EIP712 contract address into memory so it can be used later

--- src_features/signMessageEIP712/context.h
+++ src_features/signMessageEIP712/context.h
@@ -4,12 +4,14 @@
 #ifdef HAVE_EIP712_FULL_SUPPORT
 
 #include <stdbool.h>
+#include "ethUstream.h" // ADDRESS_LENGTH
 
 typedef struct
 {
     uint8_t *typenames_array;
     uint8_t *structs_array;
     uint8_t *current_struct_fields_array;
+    uint8_t contract_addr[ADDRESS_LENGTH];
 }   s_eip712_context;
 
 extern s_eip712_context *eip712_context;

d2659ed33273ea2130e187611eb674ae25e3e641
Alexandre Paillier <alexandre.paillier@ledger.fr>
2022-10-14 17:40:14 +0200

    Version bump to 1.10.0

--- tests/speculos/test_configuration_cmd.py
+++ tests/speculos/test_configuration_cmd.py
@@ -1,10 +1,10 @@
 
 def test_configuration(cmd):
     if cmd.model == "nanos":
-        assert cmd.get_configuration() == (14, 1, 9, 20)
-    
+        assert cmd.get_configuration() == (14, 1, 10, 0)
+
     if cmd.model == "nanox":
-        assert cmd.get_configuration() == (14, 1, 9, 20)
+        assert cmd.get_configuration() == (14, 1, 10, 0)
 
     if cmd.model == "nanosp":
-        assert cmd.get_configuration() == (14, 1, 9, 20)
+        assert cmd.get_configuration() == (14, 1, 10, 0)

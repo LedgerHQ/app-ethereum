c95116abede2fe10194322b69ea2a82af83afc0a
Leandre <leandre@qq.com>
2022-11-07 18:02:19 +0800

    Add KCC(KUCOIN Chain) with ChainID = 321

--- src_common/network.c
+++ src_common/network.c
@@ -48,7 +48,8 @@ const network_info_t NETWORK_MAPPING[] = {
     {.chain_id = 7341, .name = "Shyft", .ticker = "SHFT "},
     {.chain_id = 19, .name = "Songbird", .ticker = "SGB "},
     {.chain_id = 73799, .name = "Volta", .ticker = "VOLTA "},
-    {.chain_id = 25, .name = "Cronos", .ticker = "CRO "}};
+    {.chain_id = 25, .name = "Cronos", .ticker = "CRO "}},
+    {.chain_id = 321, .name = "KCC", .ticker = "KCS "}};
 
 uint64_t get_chain_id(void) {
     uint64_t chain_id = 0;

b868bde538943b753ef5103e70a68c50753d649b
Leandre <leandre@qq.com>
2022-11-24 18:39:00 +0800

    Update network.c

--- src_common/network.c
+++ src_common/network.c
@@ -52,7 +52,7 @@ const network_info_t NETWORK_MAPPING[] = {
     {.chain_id = 321, .name = "KCC", .ticker = "KCS "},
     {.chain_id = 534354, .name = "Scroll (Pre-Alpha)", .ticker = "SCR "},
     {.chain_id = 534353, .name = "Scroll (Goerli)", .ticker = "SCR "},
-    {.chain_id = 534352, .name = "Scroll", .ticker = "SCR "};
+    {.chain_id = 534352, .name = "Scroll", .ticker = "SCR "}};
 
 uint64_t get_chain_id(void) {
     uint64_t chain_id = 0;

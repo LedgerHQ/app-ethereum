fd9d2cce8d6efd4e27e3bb265fee49c4b470c629
Alexandre Paillier <alexandre.paillier@ledger.fr>
2022-11-25 16:40:15 +0100

    Moved hash_byte & hash_nbytes functions to common

--- src_common/hash_bytes.h
+++ src_common/hash_bytes.h
@@ -1,13 +1,10 @@
 #ifndef HASH_BYTES_H_
 #define HASH_BYTES_H_
 
-#ifdef HAVE_EIP712_FULL_SUPPORT
-
+#include <stdint.h>
 #include "cx.h"
 
 void hash_nbytes(const uint8_t *const bytes_ptr, uint8_t n, cx_hash_t *hash_ctx);
 void hash_byte(uint8_t byte, cx_hash_t *hash_ctx);
-
-#endif  // HAVE_EIP712_FULL_SUPPORT
 
 #endif  // HASH_BYTES_H_

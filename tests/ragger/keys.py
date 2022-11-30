19724649db558d95aaa1e0fcb31ddd2d284418ac
Alexandre Paillier <alexandre.paillier@ledger.fr>
2022-11-30 11:39:06 +0100

    Ragger tests can now handle multiple signing keys

---
+++ tests/ragger/keys.py
@@ -0,0 +1,22 @@
+import os
+import hashlib
+from ecdsa.util import sigencode_der
+from ecdsa import SigningKey
+from enum import Enum, auto
+
+class Key(Enum):
+    CAL = auto()
+
+_keys: dict[Key, SigningKey] = dict()
+
+def _init_key(key: Key):
+    global _keys
+    with open("%s/keys/%s.pem" % (os.path.dirname(__file__), key.name.lower())) as pem_file:
+        _keys[key] = SigningKey.from_pem(pem_file.read(), hashlib.sha256)
+    assert (key in _keys) and (_keys[key] != None)
+
+def key_sign(key: Key, data: bytes) -> bytes:
+    global _keys
+    if key not in _keys:
+        _init_key(key)
+    return _keys[key].sign_deterministic(data, sigencode=sigencode_der)

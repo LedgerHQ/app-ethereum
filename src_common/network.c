1af2f8288c309dde5d4061dbdf2e9dd2b0f0bee1
vforgeoux-ledger <victor.forgeoux@ledger.fr>
2024-01-02 15:09:16 +0100

    Add Numbers network (44'/60')

--- src_common/network.c
+++ src_common/network.c
@@ -79,6 +79,7 @@ static const network_info_t NETWORK_MAPPING[] = {
     {.chain_id = 1907, .name = "Bitcichain", .ticker = "BITCI"},
     {.chain_id = 1116, .name = "Core", .ticker = "CORE"},
     {.chain_id = 7171, .name = "Bitrock Mainnet", .ticker = "BROCK"},
+    {.chain_id = 10507, .name = "Numbers Protocol", .ticker = "NUM "},
 };
 
 static const network_info_t *get_network_from_chain_id(const uint64_t *chain_id) {

d83ae9377a301c738c1fd3c3a525bc2949f83674
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2024-04-16 21:44:58 +0200

    Port scenario review in 'test_eip191'

--- tests/ragger/test_eip191.py
+++ tests/ragger/test_eip191.py
@@ -1,140 +1,78 @@
 from pathlib import Path
 import pytest
 
-from client.client import EthAppClient, StatusWord
-import client.response_parser as ResponseParser
-from client.utils import recover_message
-
 from ragger.error import ExceptionRAPDU
 from ragger.backend import BackendInterface
 from ragger.firmware import Firmware
-from ragger.navigator import Navigator, NavInsID
+from ragger.navigator.navigation_scenario import NavigateWithScenario
+
+from client.client import EthAppClient, StatusWord
+import client.response_parser as ResponseParser
+from client.utils import recover_message
 
 
 BIP32_PATH = "m/44'/60'/0'/0/0"
 
 
-def test_personal_sign_metamask(firmware: Firmware,
-                                backend: BackendInterface,
-                                navigator: Navigator,
+def common(backend: BackendInterface,
+           scenario: NavigateWithScenario,
+           test_name: str,
+           screenshot_path: Path,
+           msg: str):
+
+    app_client = EthAppClient(backend)
+
+    with app_client.get_public_addr(display=False):
+        pass
+    _, DEVICE_ADDR, _ = ResponseParser.pk_addr(app_client.response().data)
+
+    with app_client.personal_sign(BIP32_PATH, msg.encode('utf-8')):
+        scenario.review_approve(screenshot_path, test_name, "Sign")
+
+    # verify signature
+    vrs = ResponseParser.signature(app_client.response().data)
+    addr = recover_message(msg.encode('utf-8'), vrs)
+    assert addr == DEVICE_ADDR
+
+
+def test_personal_sign_metamask(backend: BackendInterface,
+                                scenario_navigator: NavigateWithScenario,
                                 test_name: str,
                                 default_screenshot_path: Path):
 
     msg = "Example `personal_sign` message"
-
-    app_client = EthAppClient(backend)
-
-    with app_client.get_public_addr(display=False):
-        pass
-    _, DEVICE_ADDR, _ = ResponseParser.pk_addr(app_client.response().data)
-
-    with app_client.personal_sign(BIP32_PATH, msg.encode('utf-8')):
-        if firmware.device.startswith("nano"):
-            next_action = NavInsID.RIGHT_CLICK
-            confirm_action = NavInsID.BOTH_CLICK
-            initial_instructions = [NavInsID.RIGHT_CLICK]
-            # Skip 1st screen because 'Sign' is already present
-            navigator.navigate(initial_instructions,
-                               screen_change_after_last_instruction=False)
-        else:
-            next_action = NavInsID.USE_CASE_REVIEW_TAP
-            confirm_action = NavInsID.USE_CASE_REVIEW_CONFIRM
-
-        navigator.navigate_until_text_and_compare(next_action,
-                                                  [confirm_action],
-                                                  "Sign",
-                                                  default_screenshot_path,
-                                                  test_name)
-
-    # verify signature
-    vrs = ResponseParser.signature(app_client.response().data)
-    addr = recover_message(msg.encode('utf-8'), vrs)
-    assert addr == DEVICE_ADDR
+    common(backend, scenario_navigator, test_name, default_screenshot_path, msg)
 
 
-def test_personal_sign_non_ascii(firmware: Firmware,
-                                 backend: BackendInterface,
-                                 navigator: Navigator,
+def test_personal_sign_non_ascii(backend: BackendInterface,
+                                 scenario_navigator: NavigateWithScenario,
                                  test_name: str,
                                  default_screenshot_path: Path):
 
     msg = "0x9c22ff5f21f0b81b113e63f7db6da94fedef11b2119b4088b89664fb9a3cb658"
-
-    app_client = EthAppClient(backend)
-
-    with app_client.get_public_addr(display=False):
-        pass
-    _, DEVICE_ADDR, _ = ResponseParser.pk_addr(app_client.response().data)
-
-    with app_client.personal_sign(BIP32_PATH, msg.encode('utf-8')):
-        if firmware.device.startswith("nano"):
-            next_action = NavInsID.RIGHT_CLICK
-            confirm_action = NavInsID.BOTH_CLICK
-            initial_instructions = [NavInsID.RIGHT_CLICK]
-            # Skip 1st screen because 'Sign' is already present
-            navigator.navigate(initial_instructions,
-                               screen_change_before_first_instruction=False,
-                               screen_change_after_last_instruction=False)
-        else:
-            next_action = NavInsID.USE_CASE_REVIEW_TAP
-            confirm_action = NavInsID.USE_CASE_REVIEW_CONFIRM
-
-        navigator.navigate_until_text_and_compare(next_action,
-                                                  [confirm_action],
-                                                  "Sign",
-                                                  default_screenshot_path,
-                                                  test_name)
-
-    # verify signature
-    vrs = ResponseParser.signature(app_client.response().data)
-    addr = recover_message(msg.encode('utf-8'), vrs)
-    assert addr == DEVICE_ADDR
+    common(backend, scenario_navigator, test_name, default_screenshot_path, msg)
 
 
 def test_personal_sign_opensea(firmware: Firmware,
                                backend: BackendInterface,
-                               navigator: Navigator,
+                               scenario_navigator: NavigateWithScenario,
                                test_name: str,
                                default_screenshot_path: Path):
 
-    msg = "Welcome to OpenSea!\n\nClick to sign in and accept the OpenSea Terms of Service: https://opensea.io/tos\n\nThis request will not trigger a blockchain transaction or cost any gas fees.\n\nYour authentication status will reset after 24 hours.\n\nWallet address:\n0x9858effd232b4033e47d90003d41ec34ecaeda94\n\nNonce:\n2b02c8a0-f74f-4554-9821-a28054dc9121"
-
-    app_client = EthAppClient(backend)
-
-    with app_client.get_public_addr(display=False):
-        pass
-    _, DEVICE_ADDR, _ = ResponseParser.pk_addr(app_client.response().data)
-
     if firmware.device == "nanos":
         pytest.skip("Not supported on LNS")
-    with app_client.personal_sign(BIP32_PATH, msg.encode('utf-8')):
-        if firmware.device.startswith("nano"):
-            next_action = NavInsID.RIGHT_CLICK
-            confirm_action = NavInsID.BOTH_CLICK
-            initial_instructions = [NavInsID.RIGHT_CLICK]
-            # Skip 1st screen because 'Sign' is already present
-            navigator.navigate(initial_instructions,
-                               screen_change_before_first_instruction=False,
-                               screen_change_after_last_instruction=False)
-        else:
-            next_action = NavInsID.USE_CASE_REVIEW_TAP
-            confirm_action = NavInsID.USE_CASE_REVIEW_CONFIRM
 
-        navigator.navigate_until_text_and_compare(next_action,
-                                                  [confirm_action],
-                                                  "Sign",
-                                                  default_screenshot_path,
-                                                  test_name)
-
-    # verify signature
-    vrs = ResponseParser.signature(app_client.response().data)
-    addr = recover_message(msg.encode('utf-8'), vrs)
-    assert addr == DEVICE_ADDR
+    msg = "Welcome to OpenSea!\n\n"
+    msg += "Click to sign in and accept the OpenSea Terms of Service: https://opensea.io/tos\n\n"
+    msg += "This request will not trigger a blockchain transaction or cost any gas fees.\n\n"
+    msg += "Your authentication status will reset after 24 hours.\n\n"
+    msg += "Wallet address:\n0x9858effd232b4033e47d90003d41ec34ecaeda94\n\nNonce:\n2b02c8a0-f74f-4554-9821-a28054dc9121"
+    common(backend, scenario_navigator, test_name, default_screenshot_path, msg)
 
 
 def test_personal_sign_reject(firmware: Firmware,
                               backend: BackendInterface,
-                              navigator: Navigator,
+                              scenario_navigator: NavigateWithScenario,
                               test_name: str,
                               default_screenshot_path: Path):
 
@@ -145,21 +83,10 @@ def test_personal_sign_reject(firmware: Firmware,
     try:
         with app_client.personal_sign(BIP32_PATH, msg.encode('utf-8')):
             if firmware.device.startswith("nano"):
-                next_action = NavInsID.RIGHT_CLICK
-                confirm_action = NavInsID.BOTH_CLICK
-                navigator.navigate_until_text_and_compare(next_action,
-                                                          [confirm_action],
-                                                          "Cancel",
-                                                          default_screenshot_path,
-                                                          test_name)
+                end_text = "Cancel"
             else:
-                # instructions = [NavInsID.USE_CASE_REVIEW_TAP]
-                instructions = [NavInsID.USE_CASE_CHOICE_REJECT,
-                                NavInsID.USE_CASE_CHOICE_CONFIRM,
-                                NavInsID.USE_CASE_STATUS_DISMISS]
-                navigator.navigate_and_compare(default_screenshot_path,
-                                               test_name,
-                                               instructions)
+                end_text = "Sign"
+            scenario_navigator.review_reject(default_screenshot_path, test_name, end_text)
 
     except ExceptionRAPDU as e:
         assert e.status == StatusWord.CONDITION_NOT_SATISFIED

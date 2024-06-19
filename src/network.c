d3b6eae5557759cc300f3caffdbd00ae7ae94856
Ivan Schasny <31857042+ischasny@users.noreply.github.com>
2024-06-19 12:24:17 +0100

    Update network.c
    Include zkSync Sepolia and zkSync Mainnet

--- src/network.c
+++ src/network.c
@@ -86,6 +86,8 @@ static const network_info_t NETWORK_MAPPING[] = {
     {.chain_id = 59144, .name = "Linea", .ticker = "ETH"},
     {.chain_id = 11155111, .name = "Sepolia", .ticker = "ETH"},
     {.chain_id = 17000, .name = "Holesky", .ticker = "ETH"},
+    {.chain_id = 324, .name = "zkSync Mainnet", .ticker = "ETH"},
+    {.chain_id = 300, .name = "zkSync Sepolia", .ticker = "ETH"},
 };
 
 static const network_info_t *get_network_from_chain_id(const uint64_t *chain_id) {

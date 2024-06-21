b7af62dc13a385439edf49bbf49fcaecedb783e1
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2024-06-21 14:02:26 +0200

    Remove useless functions

--- src_nbgl/ui_message_signing.h
+++ src_nbgl/ui_message_signing.h
@@ -9,11 +9,6 @@
 #define TEXT_REVIEW_EIP712 REVIEW(TEXT_TYPED_MESSAGE)
 #define TEXT_SIGN_EIP712   SIGN(TEXT_TYPED_MESSAGE)
 
-void ui_message_review_choice(bool confirm);
-void ui_message_start(const char *title,
-                      void (*start_func)(void),
-                      void (*approved_func)(void),
-                      void (*rejected_func)(void));
 void ui_typed_message_review_choice(bool confirm);
 
 #endif  // UI_MESSAGE_SIGNING_H_

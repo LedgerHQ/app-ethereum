533bda2dea9e2c28e2e6f654fb25de280d701931
Alexandre Paillier <alexandre.paillier@ledger.fr>
2024-10-21 13:50:48 +0200

    Support for new TX sign modes - WIP

--- src_features/signTx/ethUstream.h
+++ src_features/signTx/ethUstream.h
@@ -118,9 +118,14 @@ typedef struct txContext_t {
     uint32_t commandLength;
     txContent_t *content;
     uint8_t txType;
+    bool rlp_size_known;
+    uint32_t remaining_rlp_size;
+#ifdef HAVE_GENERIC_TX_PARSER
+    bool store_calldata;
+#endif
 } txContext_t;
 
-bool init_tx(txContext_t *context, cx_sha3_t *sha3, txContent_t *content);
+bool init_tx(txContext_t *context, cx_sha3_t *sha3, txContent_t *content, bool store_calldata);
 parserStatus_e process_tx(txContext_t *context, const uint8_t *buffer, size_t length);
 parserStatus_e continueTx(txContext_t *context);
 bool copyTxData(txContext_t *context, uint8_t *out, uint32_t length);

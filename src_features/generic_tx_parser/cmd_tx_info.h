3811c4c192c91c29256a405b265f207b6057d877
Baptistin BOILOT <baptistin.boilot@ledger.fr>
2024-12-03 17:48:40 +0100

    test: add gcs fuzzer

--- src_features/generic_tx_parser/cmd_tx_info.h
+++ src_features/generic_tx_parser/cmd_tx_info.h
@@ -2,7 +2,9 @@
 #define CMD_TX_INFO_H_
 
 #include <stdint.h>
+#include "gtp_tx_info.h"
 
+ bool verify_struct(const s_tx_info_ctx *context);
 uint16_t handle_tx_info(uint8_t p1, uint8_t p2, uint8_t lc, const uint8_t *payload);
 void gcs_cleanup(void);
 

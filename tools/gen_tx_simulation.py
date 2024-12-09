cc52e55312f91c6aa5b85dad3e09b7a9e6aec2d3
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2024-12-09 16:42:53 +0100

    tool

--- tools/gen_tx_simulation.py
+++ tools/gen_tx_simulation.py
@@ -17,6 +17,10 @@ from tlv import format_tlv  # type: ignore
 from keychain import Key, sign_data   # type: ignore
 
 
+class P1Type(IntEnum):
+    NORMAL = 0x00
+    DEMO = 0x01
+
 class TxSimulationTag(IntEnum):
     STRUCTURE_TYPE = 0x01
     STRUCTURE_VERSION = 0x02
@@ -47,6 +51,7 @@ def init_parser() -> argparse.ArgumentParser:
     parser.add_argument("--category", "-C", type=int, required=True, help="Category")
     parser.add_argument("--message", "-M", required=True, help="Provider Message")
     parser.add_argument("--url", "-U", required=True, help="Report Tiny URL")
+    parser.add_argument("--demo", "-d", action='store_true', help="Set demo mode to just display the related screens, without real Tx")
     parser.add_argument("--verbose", "-v", action='store_true', help="Verbose mode")
     return parser
 
@@ -68,11 +73,12 @@ def set_logging(verbose: bool = False) -> None:
 # ===============================================================================
 #          Prepare APDU - Extracted from python client
 # ===============================================================================
-def serialize(cdata: bytes) -> bytes:
+def serialize(p1: P1Type, cdata: bytes) -> bytes:
     """
     Serializes the TX Simulation information into byte format.
 
     Args:
+        p1 (P1Type): P1 parameter to be included in the header.
         cdata (bytes): The TX Simulation data to be serialized.
 
     Returns:
@@ -83,7 +89,7 @@ def serialize(cdata: bytes) -> bytes:
     header = bytearray()
     header.append(CLA)
     header.append(PROVIDE_TX_SIMULATION)
-    header.append(0x00)
+    header.append(p1)
     header.append(0x00)
     header.append(len(cdata))
     # Return the concatenation of the header and cdata
@@ -110,10 +116,11 @@ def generate_tlv_payload(risk: int,
     """
 
     # Construct the TLV payload
+    tx_hash = bytes.fromhex("00"*32) #if chain_id == 0 else bytes.fromhex("deadbeaf"*8)
     payload: bytes = format_tlv(TxSimulationTag.STRUCTURE_TYPE, 9)
     payload += format_tlv(TxSimulationTag.STRUCTURE_VERSION, 1)
     payload += format_tlv(TxSimulationTag.CHAIN_ID, chain_id.to_bytes(8, 'big'))
-    payload += format_tlv(TxSimulationTag.TX_HASH, bytes.fromhex("deadbeaf"*8))
+    payload += format_tlv(TxSimulationTag.TX_HASH, tx_hash)
     payload += format_tlv(TxSimulationTag.W3C_NORMALIZED_RISK, risk.to_bytes(2, 'big'))
     payload += format_tlv(TxSimulationTag.W3C_NORMALIZED_CATEGORY, category)
     payload += format_tlv(TxSimulationTag.W3C_PROVIDER_MSG, message.encode('utf-8'))
@@ -152,7 +159,9 @@ def main() -> None:
     set_logging(args.verbose)
 
     payload = generate_tlv_payload(args.risk, args.category, args.message, args.url, args.chainid)
-    logger.info(f"TX Simulation[{len(payload)}]: {payload.hex()}")
+    logger.info(f"TX Simulation Payload[{len(payload)}]: {payload.hex()}")
+    p1 = P1Type.DEMO if args.demo else P1Type.NORMAL
+    logger.info(f"TX Simulation APDU: {serialize(p1, payload).hex()}")
 
 
 if __name__ == "__main__":

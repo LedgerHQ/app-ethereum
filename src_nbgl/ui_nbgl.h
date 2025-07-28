9b437ef2f890a923afa230f0a625ebaf6063bcd7
Alexandre Paillier <alexandre.paillier@ledger.fr>
2025-07-28 11:59:47 +0200

    WIP port

--- src_nbgl/ui_nbgl.h
+++ src_nbgl/ui_nbgl.h
@@ -4,12 +4,21 @@
 #include "shared_context.h"
 #include "glyphs.h"
 
+#ifndef LARGE_REVIEW_ICON
+    // TODO: SDK should really define this
+    #ifdef TARGET_APEX
+        #define LARGE_REVIEW_ICON C_Review_48px
+    #else
+        #define LARGE_REVIEW_ICON C_Review_64px
+    #endif
+#endif
+
 #ifdef SCREEN_SIZE_WALLET
-#define ICON_APP_WARNING C_Warning_64px
-#define ICON_APP_REVIEW  C_Review_64px
+    #define ICON_APP_WARNING LARGE_WARNING_ICON
+    #define ICON_APP_REVIEW  LARGE_REVIEW_ICON
 #else
-#define ICON_APP_WARNING C_icon_warning
-#define ICON_APP_REVIEW  C_icon_certificate
+    #define ICON_APP_WARNING C_icon_warning
+    #define ICON_APP_REVIEW  C_icon_certificate
 #endif
 
 const nbgl_icon_details_t* get_app_icon(bool caller_icon);

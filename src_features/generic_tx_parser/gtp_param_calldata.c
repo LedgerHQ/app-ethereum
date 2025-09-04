bb0081fa4eebe4a254d9d13a5439f230b9b29561
Alexandre Paillier <alexandre.paillier@ledger.fr>
2025-09-04 17:28:20 +0200

    WIP7

--- src_features/generic_tx_parser/gtp_param_calldata.c
+++ src_features/generic_tx_parser/gtp_param_calldata.c
@@ -116,85 +116,102 @@ bool format_param_calldata(const s_param_calldata *param, const char *name) {
     s_parsed_value_collection contract_addrs = {0};
     s_parsed_value_collection chain_ids = {0};
     s_parsed_value_collection selectors = {0};
+    s_parsed_value_collection amounts = {0};
+    s_parsed_value_collection spenders = {0};
 
     (void) name;
     if ((ret = value_get(&param->calldata, &calldatas))) {
         if ((ret = value_get(&param->contract_addr, &contract_addrs)) && (contract_addrs.size == calldatas.size)) {
-            if (!param->has_chain_id || ((ret = value_get(&param->chain_id, &chain_ids)) && (chain_ids.size == calldatas.size))) {
-                if (!param->has_selector || ((ret = value_get(&param->selector, &selectors)) && (selectors.size == calldatas.size))) {
-                    for (int i = 0; i < calldatas.size; ++i) {
-                        if (calldatas.value[i].length > 0) {
-                            if (get_tx_ctx_count() == 1) {
-                                ret = false;
-                                break;
-                            }
-                            uint8_t contract_addr[ADDRESS_LENGTH];
-                            uint8_t selector[CALLDATA_SELECTOR_SIZE];
-                            const uint8_t *calldata_buf;
-                            size_t calldata_length;
-                            uint64_t chain_id;
-                            uint8_t chain_id_buf[sizeof(chain_id)];
-                            s_calldata *calldata;
-
-                            if (param->has_chain_id) {
-                                buf_shrink_expand(chain_ids.value[i].ptr,
-                                                  chain_ids.value[i].length,
-                                                  chain_id_buf,
-                                                  sizeof(chain_id_buf));
-                                chain_id = read_u64_be(chain_id_buf, 0);
-                            } else {
-                                const s_tx_info *tx_info = get_current_tx_info();
-                                if (tx_info == NULL) {
-                                    ret = false;
-                                    break;
-                                }
-                                chain_id = tx_info->chain_id;
-                            }
-
-                            if (param->has_selector) {
-                                buf_shrink_expand(selectors.value[i].ptr, selectors.value[i].length, selector, sizeof(selector));
-                                calldata_buf = calldatas.value[i].ptr;
-                                calldata_length = calldatas.value[i].length;
-                            } else {
-                                if (calldatas.value[i].length < CALLDATA_SELECTOR_SIZE) {
-                                    ret = false;
-                                    break;
-                                }
-                                memcpy(selector, calldatas.value[i].ptr, CALLDATA_SELECTOR_SIZE);
-                                calldata_buf = calldatas.value[i].ptr + CALLDATA_SELECTOR_SIZE;
-                                calldata_length = calldatas.value[i].length - CALLDATA_SELECTOR_SIZE;
-                            }
-
-                            buf_shrink_expand(contract_addrs.value[i].ptr,
-                                              contract_addrs.value[i].length,
-                                              contract_addr,
-                                              sizeof(contract_addr));
-                            if (!find_matching_tx_ctx(contract_addr, selector, &chain_id)) {
-                                ret = false;
-                                break;
-                            }
-                            if (((calldata = calldata_init(calldata_length, selector)) == NULL) ||
-                                 !calldata_append(calldata, calldata_buf, calldata_length)) {
-                                ret = false;
-                                break;
-                            }
-                            if (!set_calldata_into_tx_ctx(calldata)) {
-                                delete_calldata(calldata);
-                                ret = false;
-                                break;
-                            }
-                            const s_field_list_node *field;
-                            for (field = get_fields_list();
-                                 field != NULL;
-                                 field = (const s_field_list_node*) ((s_flist_node *) field)->next) {
-                                if (!format_field(&field->field)) break;
-                            }
-                            if (field != NULL) {
-                                ret = false;
-                                break;
-                            }
-                            tx_ctx_move_to_parent();
+            if ((!param->has_chain_id || ((ret = value_get(&param->chain_id, &chain_ids)) && (chain_ids.size == calldatas.size))) &&
+                (!param->has_selector || ((ret = value_get(&param->selector, &selectors)) && (selectors.size == calldatas.size))) &&
+                (!param->has_amount || ((ret = value_get(&param->amount, &amounts)) && (amounts.size == calldatas.size))) &&
+                (!param->has_spender || ((ret = value_get(&param->spender, &spenders)) && (spenders.size == calldatas.size)))) {
+                for (int i = 0; i < calldatas.size; ++i) {
+                    if (calldatas.value[i].length > 0) {
+                        if (get_tx_ctx_count() == 1) {
+                            ret = false;
+                            break;
                         }
+                        uint8_t contract_addr[ADDRESS_LENGTH];
+                        uint8_t selector[CALLDATA_SELECTOR_SIZE];
+                        const uint8_t *calldata_buf;
+                        size_t calldata_length;
+                        uint64_t chain_id;
+                        uint8_t chain_id_buf[sizeof(chain_id)];
+                        s_calldata *calldata;
+
+                        if (param->has_chain_id) {
+                            buf_shrink_expand(chain_ids.value[i].ptr,
+                                              chain_ids.value[i].length,
+                                              chain_id_buf,
+                                              sizeof(chain_id_buf));
+                            chain_id = read_u64_be(chain_id_buf, 0);
+                        } else {
+                            const s_tx_info *tx_info = get_current_tx_info();
+                            if (tx_info == NULL) {
+                                ret = false;
+                                break;
+                            }
+                            chain_id = tx_info->chain_id;
+                        }
+
+                        if (param->has_selector) {
+                            buf_shrink_expand(selectors.value[i].ptr, selectors.value[i].length, selector, sizeof(selector));
+                            calldata_buf = calldatas.value[i].ptr;
+                            calldata_length = calldatas.value[i].length;
+                        } else {
+                            if (calldatas.value[i].length < CALLDATA_SELECTOR_SIZE) {
+                                ret = false;
+                                break;
+                            }
+                            memcpy(selector, calldatas.value[i].ptr, CALLDATA_SELECTOR_SIZE);
+                            calldata_buf = calldatas.value[i].ptr + CALLDATA_SELECTOR_SIZE;
+                            calldata_length = calldatas.value[i].length - CALLDATA_SELECTOR_SIZE;
+                        }
+
+                        buf_shrink_expand(contract_addrs.value[i].ptr,
+                                          contract_addrs.value[i].length,
+                                          contract_addr,
+                                          sizeof(contract_addr));
+                        if (!find_matching_tx_ctx(contract_addr, selector, &chain_id)) {
+                            ret = false;
+                            break;
+                        }
+
+                        // populate tx context
+                        set_tx_ctx_to(contract_addr);
+                        if (param->has_amount) {
+                            uint8_t amount[INT256_LENGTH];
+                            buf_shrink_expand(amounts.value[i].ptr, amounts.value[i].length, amount, sizeof(amount));
+                            set_tx_ctx_amount(amount);
+                        }
+                        if (param->has_spender) {
+                            uint8_t from[ADDRESS_LENGTH];
+                            buf_shrink_expand(spenders.value[i].ptr, spenders.value[i].length, from, sizeof(from));
+                            set_tx_ctx_from(from);
+                        }
+
+                        if (((calldata = calldata_init(calldata_length, selector)) == NULL) ||
+                             !calldata_append(calldata, calldata_buf, calldata_length)) {
+                            ret = false;
+                            break;
+                        }
+                        if (!set_calldata_into_tx_ctx(calldata)) {
+                            delete_calldata(calldata);
+                            ret = false;
+                            break;
+                        }
+                        const s_field_list_node *field;
+                        for (field = get_fields_list();
+                             field != NULL;
+                             field = (const s_field_list_node*) ((s_flist_node *) field)->next) {
+                            if (!format_field(&field->field)) break;
+                        }
+                        if (field != NULL) {
+                            ret = false;
+                            break;
+                        }
+                        tx_ctx_move_to_parent();
                     }
                 }
             }

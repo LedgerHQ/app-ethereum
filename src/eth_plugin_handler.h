f165932c2e96437845f1fea9f73d0de8e3f19eb4
Alexandre Paillier <alexandre.paillier@ledger.fr>
2025-12-09 15:36:35 +0100

    Changed from camelCase to snake_case in eth_plugin_handler

--- src/eth_plugin_handler.h
+++ src/eth_plugin_handler.h
@@ -9,25 +9,25 @@
 
 void eth_plugin_prepare_init(ethPluginInitContract_t *init,
                              const uint8_t *selector,
-                             uint32_t dataSize);
-void eth_plugin_prepare_provide_parameter(ethPluginProvideParameter_t *provideParameter,
+                             uint32_t data_size);
+void eth_plugin_prepare_provide_parameter(ethPluginProvideParameter_t *provide_parameter,
                                           uint8_t *parameter,
-                                          uint32_t parameterOffset);
+                                          uint32_t parameter_offset);
 void eth_plugin_prepare_finalize(ethPluginFinalize_t *finalize);
-void eth_plugin_prepare_provide_info(ethPluginProvideInfo_t *provideToken);
-void eth_plugin_prepare_query_contract_ID(ethQueryContractID_t *queryContractID,
+void eth_plugin_prepare_provide_info(ethPluginProvideInfo_t *provide_token);
+void eth_plugin_prepare_query_contract_id(ethQueryContractID_t *query_contract_id,
                                           char *name,
-                                          uint32_t nameLength,
+                                          uint32_t name_length,
                                           char *version,
-                                          uint32_t versionLength);
-void eth_plugin_prepare_query_contract_UI(ethQueryContractUI_t *queryContractUI,
-                                          uint8_t screenIndex,
+                                          uint32_t version_length);
+void eth_plugin_prepare_query_contract_ui(ethQueryContractUI_t *query_contract_ui,
+                                          uint8_t screen_index,
                                           char *title,
-                                          uint32_t titleLength,
+                                          uint32_t title_length,
                                           char *msg,
-                                          uint32_t msgLength);
+                                          uint32_t msg_length);
 
-eth_plugin_result_t eth_plugin_perform_init(uint8_t *contractAddress,
+eth_plugin_result_t eth_plugin_perform_init(uint8_t *contract_address,
                                             ethPluginInitContract_t *init);
 // NULL for cached address, or base contract address
 eth_plugin_result_t eth_plugin_call(int method, void *parameter);

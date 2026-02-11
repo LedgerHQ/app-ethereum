bd45651a5057d72fa053c573a507fcf1c91b6360
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2026-02-11 19:17:29 +0100

    Migrate to lib_tlv - Dynamic Network

--- src/features/provide_network_info/cmd_network_info.h
+++ src/features/provide_network_info/cmd_network_info.h
@@ -8,5 +8,3 @@ uint16_t handle_network_info(uint8_t p1,
                              const uint8_t *data,
                              uint8_t length,
                              unsigned int *tx);
-
-void network_info_cleanup(network_info_t *network);

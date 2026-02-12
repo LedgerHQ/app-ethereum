7c231d5d23896638aa0aae48e22de0b49ff1ef07
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2026-02-12 15:20:30 +0100

    Migrate to lib_tlv - enum_value

--- src/features/provide_enum_value/enum_value.c
+++ src/features/provide_enum_value/enum_value.c
@@ -1,156 +1,228 @@
 #include "enum_value.h"
-#include "read.h"
 #include "public_keys.h"
 #include "utils.h"
 #include "ui_utils.h"
 #include "mem.h"
 #include "proxy_info.h"
 #include "hash_bytes.h"
+#include "tlv_apdu.h"
 
-enum {
-    TAG_VERSION = 0x00,
-    TAG_CHAIN_ID = 0x01,
-    TAG_CONTRACT_ADDR = 0x02,
-    TAG_SELECTOR = 0x03,
-    TAG_ID = 0x04,
-    TAG_VALUE = 0x05,
-    TAG_NAME = 0x06,
-    TAG_SIGNATURE = 0xff,
-};
+#define STRUCT_VERSION 0x01
 
 static s_enum_value_entry *g_enum_value_list = NULL;
 
-static bool handle_version(const s_tlv_data *data, s_enum_value_ctx *context) {
-    if (data->length != sizeof(context->enum_value.version)) {
+/**
+ * @brief Parse the VERSION value.
+ *
+ * @param[in] data the tlv data
+ * @param[in] context Enum value context
+ * @return whether the handling was successful
+ */
+static bool handle_version(const tlv_data_t *data, s_enum_value_ctx *context) {
+    UNUSED(context);
+    return tlv_check_struct_version(data, STRUCT_VERSION);
+}
+
+/**
+ * @brief Parse the CHAIN_ID value.
+ *
+ * @param[in] data the tlv data
+ * @param[in] context Enum value context
+ * @return whether the handling was successful
+ */
+static bool handle_chain_id(const tlv_data_t *data, s_enum_value_ctx *context) {
+    return tlv_get_chain_id(data, &context->entry.chain_id);
+}
+
+/**
+ * @brief Parse the CONTRACT_ADDR value.
+ *
+ * @param[in] data the tlv data
+ * @param[in] context Enum value context
+ * @return whether the handling was successful
+ */
+static bool handle_contract_addr(const tlv_data_t *data, s_enum_value_ctx *context) {
+    return tlv_get_address(data, (uint8_t *) context->entry.contract_addr, false);
+}
+
+/**
+ * @brief Parse the SELECTOR value.
+ *
+ * @param[in] data the tlv data
+ * @param[in] context Enum value context
+ * @return whether the handling was successful
+ */
+static bool handle_selector(const tlv_data_t *data, s_enum_value_ctx *context) {
+    return tlv_get_selector(data, context->entry.selector, sizeof(context->entry.selector));
+}
+
+/**
+ * @brief Parse the ID value.
+ *
+ * @param[in] data the tlv data
+ * @param[in] context Enum value context
+ * @return whether the handling was successful
+ */
+static bool handle_id(const tlv_data_t *data, s_enum_value_ctx *context) {
+    if (!tlv_get_uint8(data, &context->entry.id, 0, UINT8_MAX)) {
+        PRINTF("ID: error\n");
         return false;
     }
-    context->enum_value.version = data->value[0];
     return true;
 }
 
-static bool handle_chain_id(const s_tlv_data *data, s_enum_value_ctx *context) {
-    uint8_t buf[sizeof(context->enum_value.entry.chain_id)] = {0};
-
-    if (data->length > sizeof(buf)) {
+/**
+ * @brief Parse the VALUE value.
+ *
+ * @param[in] data the tlv data
+ * @param[in] context Enum value context
+ * @return whether the handling was successful
+ */
+static bool handle_value(const tlv_data_t *data, s_enum_value_ctx *context) {
+    if (!tlv_get_uint8(data, &context->entry.value, 0, UINT8_MAX)) {
+        PRINTF("VALUE: error\n");
         return false;
     }
-    buf_shrink_expand(data->value, data->length, buf, sizeof(buf));
-    context->enum_value.entry.chain_id = read_u64_be(buf, 0);
     return true;
 }
 
-static bool handle_contract_addr(const s_tlv_data *data, s_enum_value_ctx *context) {
-    if (data->length > sizeof(context->enum_value.entry.contract_addr)) {
+/**
+ * @brief Parse the NAME value.
+ *
+ * @param[in] data the tlv data
+ * @param[in] context Enum value context
+ * @return whether the handling was successful
+ */
+static bool handle_name(const tlv_data_t *data, s_enum_value_ctx *context) {
+    if (!get_string_from_tlv_data(data,
+                                  (char *) context->entry.name,
+                                  1,
+                                  sizeof(context->entry.name) - 1)) {
+        PRINTF("NAME: failed to extract\n");
         return false;
     }
-    buf_shrink_expand(data->value,
-                      data->length,
-                      context->enum_value.entry.contract_addr,
-                      sizeof(context->enum_value.entry.contract_addr));
     return true;
 }
 
-static bool handle_selector(const s_tlv_data *data, s_enum_value_ctx *context) {
-    if (data->length > sizeof(context->enum_value.entry.selector)) {
+/**
+ * @brief Parse the SIGNATURE value.
+ *
+ * @param[in] data the tlv data
+ * @param[in] context Enum value context
+ * @return whether the handling was successful
+ */
+static bool handle_signature(const tlv_data_t *data, s_enum_value_ctx *context) {
+    buffer_t sig = {0};
+    if (!get_buffer_from_tlv_data(data,
+                                  &sig,
+                                  ECDSA_SIGNATURE_MIN_LENGTH,
+                                  ECDSA_SIGNATURE_MAX_LENGTH)) {
+        PRINTF("SIGNATURE: failed to extract\n");
         return false;
     }
-    buf_shrink_expand(data->value,
-                      data->length,
-                      context->enum_value.entry.selector,
-                      sizeof(context->enum_value.entry.selector));
+    context->sig_size = sig.size;
+    context->sig = sig.ptr;
     return true;
 }
 
-static bool handle_id(const s_tlv_data *data, s_enum_value_ctx *context) {
-    if (data->length != sizeof(context->enum_value.entry.id)) {
-        return false;
+// Define TLV tags for Enum Value
+#define ENUM_VALUE_TAGS(X)                                               \
+    X(0x00, TAG_VERSION, handle_version, ENFORCE_UNIQUE_TAG)             \
+    X(0x01, TAG_CHAIN_ID, handle_chain_id, ENFORCE_UNIQUE_TAG)           \
+    X(0x02, TAG_CONTRACT_ADDR, handle_contract_addr, ENFORCE_UNIQUE_TAG) \
+    X(0x03, TAG_SELECTOR, handle_selector, ENFORCE_UNIQUE_TAG)           \
+    X(0x04, TAG_ID, handle_id, ENFORCE_UNIQUE_TAG)                       \
+    X(0x05, TAG_VALUE, handle_value, ENFORCE_UNIQUE_TAG)                 \
+    X(0x06, TAG_NAME, handle_name, ENFORCE_UNIQUE_TAG)                   \
+    X(0xff, TAG_SIGNATURE, handle_signature, ENFORCE_UNIQUE_TAG)
+
+// Forward declaration of common handler
+static bool enum_value_common_handler(const tlv_data_t *data, s_enum_value_ctx *context);
+
+// Generate TLV parser for Enum Value
+DEFINE_TLV_PARSER(ENUM_VALUE_TAGS, &enum_value_common_handler, enum_value_tlv_parser)
+
+/**
+ * @brief Common handler called for all tags to hash them (except signature).
+ *
+ * @param[in] data data to handle
+ * @param[out] context struct context
+ * @return whether the handling was successful
+ */
+static bool enum_value_common_handler(const tlv_data_t *data, s_enum_value_ctx *context) {
+    if (data->tag != TAG_SIGNATURE) {
+        hash_nbytes(data->raw.ptr, data->raw.size, (cx_hash_t *) &context->hash_ctx);
     }
-    context->enum_value.entry.id = data->value[0];
     return true;
 }
 
-static bool handle_value(const s_tlv_data *data, s_enum_value_ctx *context) {
-    if (data->length != sizeof(context->enum_value.entry.value)) {
-        return false;
-    }
-    context->enum_value.entry.value = data->value[0];
-    return true;
-}
-
-static bool handle_name(const s_tlv_data *data, s_enum_value_ctx *context) {
-    str_cpy_explicit_trunc((const char *) data->value,
-                           data->length,
-                           context->enum_value.entry.name,
-                           sizeof(context->enum_value.entry.name));
-    return true;
-}
-
-static bool handle_signature(const s_tlv_data *data, s_enum_value_ctx *context) {
-    if (data->length > sizeof(context->enum_value.signature)) {
-        return false;
-    }
-    context->enum_value.signature_length = data->length;
-    memcpy(context->enum_value.signature, data->value, data->length);
-    return true;
-}
-
-bool handle_enum_value_struct(const s_tlv_data *data, s_enum_value_ctx *context) {
-    bool ret;
-
-    switch (data->tag) {
-        case TAG_VERSION:
-            ret = handle_version(data, context);
-            break;
-        case TAG_CHAIN_ID:
-            ret = handle_chain_id(data, context);
-            break;
-        case TAG_CONTRACT_ADDR:
-            ret = handle_contract_addr(data, context);
-            break;
-        case TAG_SELECTOR:
-            ret = handle_selector(data, context);
-            break;
-        case TAG_ID:
-            ret = handle_id(data, context);
-            break;
-        case TAG_VALUE:
-            ret = handle_value(data, context);
-            break;
-        case TAG_NAME:
-            ret = handle_name(data, context);
-            break;
-        case TAG_SIGNATURE:
-            ret = handle_signature(data, context);
-            break;
-        default:
-            PRINTF(TLV_TAG_ERROR_MSG, data->tag);
-            ret = false;
-    }
-    if (ret && (data->tag != TAG_SIGNATURE)) {
-        if (cx_hash_no_throw((cx_hash_t *) &context->struct_hash,
-                             0,
-                             data->raw,
-                             data->raw_size,
-                             NULL,
-                             0) != CX_OK) {
-            return false;
-        }
-    }
-    return ret;
-}
-
-bool verify_enum_value_struct(const s_enum_value_ctx *context) {
+/**
+ * @brief Verify the payload signature
+ *
+ * Verify the SHA-256 hash of the payload against the public key
+ *
+ * @param[in] context struct context
+ * @return whether it was successful
+ */
+static bool verify_signature(const s_enum_value_ctx *context) {
     uint8_t hash[INT256_LENGTH];
-    s_enum_value_entry *entry;
 
-    if (finalize_hash((cx_hash_t *) &context->struct_hash, hash, sizeof(hash)) != true) {
+    if (finalize_hash((cx_hash_t *) &context->hash_ctx, hash, sizeof(hash)) != true) {
         return false;
     }
+
     if (check_signature_with_pubkey(hash,
                                     sizeof(hash),
                                     CERTIFICATE_PUBLIC_KEY_USAGE_CALLDATA,
-                                    (uint8_t *) context->enum_value.signature,
-                                    context->enum_value.signature_length) != true) {
+                                    (uint8_t *) context->sig,
+                                    context->sig_size) != true) {
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * @brief Verify the received fields
+ *
+ * Check the mandatory fields are present
+ *
+ * @param[in] context struct context
+ * @return whether it was successful
+ */
+static bool verify_fields(const s_enum_value_ctx *context) {
+    return TLV_CHECK_RECEIVED_TAGS(context->received_tags,
+                                   TAG_VERSION,
+                                   TAG_CHAIN_ID,
+                                   TAG_CONTRACT_ADDR,
+                                   TAG_SELECTOR,
+                                   TAG_ID,
+                                   TAG_VALUE,
+                                   TAG_NAME,
+                                   TAG_SIGNATURE);
+}
+
+/**
+ * @brief Wrapper to parse Enum Value TLV payload.
+ *
+ * @param[in] buf TLV buffer
+ * @param[out] context Enum value context
+ * @return whether parsing was successful
+ */
+bool handle_enum_value_tlv_payload(const buffer_t *buf, s_enum_value_ctx *context) {
+    return enum_value_tlv_parser(buf, context, &context->received_tags);
+}
+
+bool verify_enum_value_struct(const s_enum_value_ctx *context) {
+    s_enum_value_entry *entry;
+
+    if (!verify_fields(context)) {
+        PRINTF("Error: Missing mandatory fields in descriptor!\n");
+        return false;
+    }
+
+    if (!verify_signature(context)) {
+        PRINTF("Error: Signature verification failed for descriptor!\n");
         return false;
     }
 
@@ -159,7 +231,7 @@ bool verify_enum_value_struct(const s_enum_value_ctx *context) {
         return false;
     }
 
-    memcpy(entry, &context->enum_value.entry, sizeof(*entry));
+    memcpy(entry, &context->entry, sizeof(*entry));
     flist_push_back((flist_node_t **) &g_enum_value_list, (flist_node_t *) entry);
     return true;
 }
